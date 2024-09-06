eefda0f31b8d788c1f9bd65c080de0c84fdd28a1
Chandradev Sah <chandradev819@gmail.com>
2024-09-06 19:31:22 +0530

    Add project files.

---
+++ BlazorisedDemo/Model/CartItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorisedDemo.Model
+{
+    public class CartItem
+    {
+        public Guid Id { get; set; }
+        public string TestType { get; set; }
+        public long TestsPerBatch { get; set; }
+        public long TotalTests { get; set; }
+        public long TypeId { get; set; }
+        public long PriceId { get; set; }
+        public int NoOfBatches { get; set; }
+        public int Amount { get; set; }
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal ItemPrice { get; set; }
+        public string Stripepricingid { get; set; }
+        public string Name { get; set; }
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal Total { get; set; }
+        public string Region { get; set; }
+    }
+}
---
+++ BlazorisedDemo/Model/EmployeeData.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorisedDemo.Model
+{
+    public class EmployeeData
+    {
+        public Task<List<Employee>> GetDataAsync()
+        {
+            var employeeList = new List<Employee>
+        {
+            new Employee { Id = 1, FirstName = "John", LastName = "Doe", Email = "john.doe@example.com", Salary = 60000m },
+            new Employee { Id = 2, FirstName = "Jane", LastName = "Smith", Email = "jane.smith@example.com", Salary = 65000m },
+            new Employee { Id = 3, FirstName = "Sam", LastName = "Brown", Email = "sam.brown@example.com", Salary = 70000m },
+            new Employee { Id = 4, FirstName = "Emily", LastName = "Davis", Email = "emily.davis@example.com", Salary = 75000m }
+        };
+
+            return Task.FromResult(employeeList);
+        }
+    }
+}
